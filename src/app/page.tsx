925d705866b0dbaba51c121b1f3a6befceb5addb
fil <fil@akamaipos.com>
2025-01-14 17:34:19 +0800

    make the sign-in button working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -13,7 +14,9 @@ export default function Home() {
       <div className="space-y-6 bg-white p-4 rounded-md text-center">
         <h1 className={cn("text-6xl font-semibold drop-shadow-md", font.className)}>Auth</h1>
         <p className="text-lg capitalize">A simple authentication service</p>
-        <Button variant="secondary" size="lg">Sign In</Button>
+        <LoginButton>
+          <Button variant="secondary" size="lg">Sign In</Button>
+        </LoginButton>
       </div>
     </main>
   );
---
+++ src/components/auth/login-button.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface LoginButtonProps {
+    children: React.ReactNode,
+    mode: "modal" | "redirect",
+    isChild?: boolean
+}
+
+export const LoginButton = ({ children, mode = "redirect", isChild = false}: LoginButtonProps) => {
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push('/auth/login');
+    }
+
+    if (mode == 'modal') {
+        return (
+            <span>rendering modal</span>
+        );
+    }
+    return (
+        <span onClick={onClick} className="cursor-pointer">{children}</span>
+    )
+}
