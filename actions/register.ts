bd54d52768d5453cff65f7c6989f39e938e305b6
fil <fil@akamaipos.com>
2025-02-02 21:40:18 +0800

    handled and save register form data and save to database

--- actions/register.ts
+++ actions/register.ts
@@ -2,11 +2,35 @@
 
 import { RegistrationSchema } from "../schemas";
 import { z } from "zod";
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
 
 export const register = async (values: z.infer<typeof RegistrationSchema>) => {
     const validated = RegistrationSchema.safeParse(values);
+
     if (!validated.success) {
         return { error: "Something went wrong!"};
     }
-    return { success: "Email sent!"};
+
+    const { name, email, password } = validated.data;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const existingUser = await db.user.findUnique({
+        where: { email }
+    });
+
+    if (existingUser) {
+        return { error: "User is already being used!"};
+    }
+
+    const user = await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    });
+
+    return { success: "User created!"};
 }
