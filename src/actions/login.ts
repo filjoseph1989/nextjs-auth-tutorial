1374e1f59b1c1e4e8f0b61dc69d929bbf93c4501
fil <fil@akamaipos.com>
2025-02-15 18:54:52 +0800

    get rid of the console.log

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -16,23 +16,15 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     const { email, password } = validated.data;
 
     try {
-        console.log("signing in...");
         const response = await signIn("credentials", {
             email,
             password,
             redirectTo: DEFAULT_LOGIN_REDIRECT,
         });
 
-        if (response.error) {
-            console.error("error 1", response.error);
-            return { error: "Invalid credentials!" };
-        }
-
         return { success: "Logged in!" };
     } catch (error) {
-        console.error("error 2", error);
         if (error instanceof AuthError) {
-            console.error("error 3", error);
             if (error.type === "CredentialsSignin") {
                 return { error: "Invalid credentials!" };
             }
