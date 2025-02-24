6eb411d8d2ecbc94cfc43783db87fa82ed6ab1c0
fil <fil@akamaipos.com>
2025-02-24 23:27:24 +0800

    added new fields and extended the user session by adding a role property

--- src/auth.ts
+++ src/auth.ts
@@ -2,15 +2,23 @@ import NextAuth from "next-auth"
 import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "./data/user";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
     callbacks: {
         async jwt({token}) {
+            if (!token.sub) return token;
+            const user = await getUserById(token.sub);
+            if (!user) return token;
+            token.role = user.role;
             return token;
         },
         async session({session, token}) {
             if (session.user && token.sub) {
                 session.user.id = token.sub;
+            }
+            if (token.role) {
+                session.user.role = token.role;
             }
             return session;
         }
