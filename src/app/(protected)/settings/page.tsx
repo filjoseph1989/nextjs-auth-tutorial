79631e41ea6c21e619a0d6c148ef05cc682eb72d
fil <fil@akamaipos.com>
2025-02-25 19:37:17 +0800

    Extended session

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -2,6 +2,7 @@ import { auth, signOut } from "@/auth";
 
 const SettingsPage = async () => {
     const session = await auth();
+
     return (
         <div>
             <h1>Settings Page</h1>
--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
     callbacks: {
@@ -18,7 +19,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
                 session.user.id = token.sub;
             }
             if (token.role) {
-                session.user.role = token.role;
+                session.user.role = token.role as UserRole;
             }
             return session;
         }
